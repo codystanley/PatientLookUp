a9c8dae1e82c5edf7a44d53662b4465d35a2a1d3
codystanley <cody.stanley@triagelogic.com>
2024-07-04 06:44:24 -0400

    Added more fields. Fixed droopdown.

--- forms.py
+++ forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SelectField, SubmitField, validators
 from wtforms.validators import DataRequired
 
 class LookUpForm(FlaskForm):
     number = StringField('Phone Number', validators=[DataRequired()])
     dob = StringField('Date of Birth', validators=[DataRequired()])
+    gender = SelectField('Birth Gender', choices=[('male', 'Male'), ('female', 'Female')], validators=[validators.DataRequired()])
     lastName = StringField('Last Name', validators=[DataRequired()])
+    firstName = StringField('First Name')
     submit = SubmitField('Submit')
